62c7e3bee8a099c72f0d0e58672630cb33a3ce0f
danielzt94 <danieltockan@gmail.com>
2021-03-20 17:18:34 +0000

    firebase, storing registered users in firebase data base

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,7 +56,7 @@ class SignIn extends React.Component {
           <div className="buttons">
             <CustomButton type="submit">SIGN IN</ CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              SIGN IN WITH GOOGLE
+              GOOGLE SIGN IN
             </ CustomButton>
           </div>
         </form>
